639fb5c857be99f0653251485af03c2fffe0b0e2
MrBilo-PT <david_miguel_7@hotmail.com>
2025-10-14 09:09:42 +0100

    2º commit - David Working on Grid Classe - Created the grid(rows and columns) Work incomplete

--- src/Grid.java
+++ src/Grid.java
@@ -1,4 +1,18 @@
+import javax.xml.stream.Location;
+
 public class Grid {
-    public void addShip(Ship s) {
-    }
+    {
+        Location[][] grid;
+        int points;
+
+        // Constants for number of rows and columns.
+        public static final int NUM_ROWS = 10;
+        public static final int NUM_COLS = 10;
+
+    public Grid()
+        {
+            if (NUM_ROWS > 26)
+            {
+                throw new IllegalArgumentException("ERROR! NUM_ROWS CANNOT BE > 26");
+            }
 }
